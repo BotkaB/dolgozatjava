f3e85d2100f8a3bb6ec48471800d1a9dc6d78b92
BotkaB <botikabinaka@gmail.com>
2025-05-16 08:33:25 +0200

    átrendezés

--- dolgozat0415/src/main/java/hu/szamalk/modell/SzazadNagyobbMint21.java
+++
@@ -1,8 +0,0 @@
-package hu.szamalk.modell;
-
-public class SzazadNagyobbMint21 extends Exception {
-
-    public SzazadNagyobbMint21(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/hu/szamalk/modell/Szobor.java
@@ -0,0 +1,64 @@
+package hu.szamalk.modell;
+
+import java.util.Comparator;
+
+public class Szobor extends Mutargy implements Comparable<Szobor> {
+
+    private final String anyag;
+    private final int szazad;
+
+
+    public Szobor(String alkoto, String cim, Kategoriak kategoria, String anyag, int szazad) throws SzazadNagyobbMint21 {
+        super(alkoto, cim, kategoria);
+        this.anyag = anyag;
+        szazadNagyobbMint21(szazad);
+        this.szazad = szazad;
+
+
+    }
+
+    public Szobor(String alkoto, String cim, Kategoriak kategoria, String anyag) throws SzazadNagyobbMint21 {
+        this(alkoto, cim, kategoria, anyag, 20);
+    }
+
+    private void szazadNagyobbMint21(int szazad) throws SzazadNagyobbMint21 {
+        if (szazad > 21) {
+            throw new SzazadNagyobbMint21("Század nagyobb, mint 21." + szazad);
+        }
+    }
+
+    public String getAnyag() {
+        return anyag;
+    }
+
+    public int getSzazad() {
+        return szazad;
+    }
+
+    @Override
+    public int compareTo(Szobor masik) {
+        return this.getAlkoto().compareTo(masik.getAlkoto());
+    }
+
+    public class SzazadComparator implements Comparator<Szobor> {
+        @Override
+        public int compare(Szobor egyik, Szobor masik) {
+            return egyik.getSzazad() - masik.getSzazad();
+        }
+    }
+
+    public class AnyagComparator implements Comparator<Szobor> {
+        @Override
+        public int compare(Szobor egyik, Szobor masik) {
+            return egyik.getAnyag().compareTo(masik.getAnyag());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Szobor{" +
+                "anyag='" + anyag + '\'' +
+                ", szazad=" + szazad +
+                '}';
+    }
+}
